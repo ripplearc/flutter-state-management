468d598d4cfdbb5ce822c85715ef86cc8a391249
Suyang Dong <ripplearc@gmail.com>
2023-05-21 10:26:43 -0400

    - Add integration tests to verify the behavior across generator and favorites pages

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:namer_app/generator_page.dart';
 
 import 'package:namer_app/main.dart' as app;
 import 'package:namer_app/main.dart';
@@ -33,29 +34,85 @@ void main() {
         )));
   }
 
+  setUp(() async {
+    randomWords.reset();
+  });
+
   group('end-to-end test', () {
     when(mockRandomWordFactory.generateRandomWord()).thenAnswer((_) {
       return randomWords.next();
     });
-    testWidgets(
-        '''given the current word, then tap the favorite button and add a new word, 
-        should see the word with favorite icon in the history''',
+    testWidgets('''given a history word, toggle its favorite, 
+        and go to the favorites page to delete its favorite ''',
         (tester) async {
-          // Arrange: Pump the generator page
+      // Arrange: Pump the generator page
       runAppUnderTest();
       await tester.pumpAndSettle();
 
-      // Act: Tap the favorite button and add a new word
-      await tester.tap(find.byIcon(Icons.favorite_border));
+      // Act: Add the word to the history and toggle the favorite status
       await tester.tap(find.text("Next"));
+      await tester.pumpAndSettle();
+      // Act: Tap the TextButton that has text "test1"
+      await tester.tap(find.widgetWithText(SizeTransition, "test1"));
+      await tester.pump();
 
+      // Assert: should see the word with favorite icon in the history
+      final icon = find.descendant(
+          of: find.byType(AnimatedList), matching: find.byIcon(Icons.favorite));
+      expect(icon, findsOneWidget);
 
-      // Assert: First random word should be "test1"
+      // Act: Tap the favorite button to toggle the favorite status
+      await tester.tap(icon);
+      await tester.pump();
+      // Assert: should see the word being unfavorited
+      final noIcon = find.descendant(
+          of: find.byType(AnimatedList), matching: find.byIcon(Icons.favorite));
+      expect(noIcon, findsNothing);
+
+      // Act: Tap the word of "test1", and go to the favorites page
+      await tester.tap(find.widgetWithText(SizeTransition, "test1"));
+      await tester.pump();
+      await tester.tap(find.text("Favorites"));
+      await tester.pumpAndSettle();
+
+      // Assert: should see the word in the faovrites page
       expect(find.text("test1"), findsOneWidget);
-      // Assert: No history should be displayed
-      final animatedListWidget =
-          tester.widget<AnimatedList>(find.byType(AnimatedList));
-      expect(animatedListWidget.initialItemCount, 0);
+
+      // Act: Tap the delete button, and then go back to the generator page
+      await tester.tap(find.byIcon(Icons.delete));
+      await tester.tap(find.text("Home"));
+      await tester.pumpAndSettle();
+
+      // Assert: should see the word being unfavorited on the generator page
+      final noIcon2 = find.descendant(
+          of: find.byType(AnimatedList), matching: find.byIcon(Icons.favorite));
+      expect(noIcon2, findsNothing);
+    });
+
+    testWidgets("""given the current word is favorited, 
+        then go to the favorites page to delete it, 
+        should see the current word being unfavorited on the generator page""",
+        (tester) async {
+      // Arrange: Pump the generator page
+      runAppUnderTest();
+      await tester.pumpAndSettle();
+
+      // Act: Tap the favorite button
+      await tester.tap(find.byIcon(Icons.favorite_border));
+      await tester.pump();
+      // Assert: should see the word being favorited
+      expect(
+          find.widgetWithIcon(FavoriteButton, Icons.favorite), findsOneWidget);
+
+      // Act: Go to the favorites page, tap the delete button, and then go back to the generator page
+      await tester.tap(find.text("Favorites"));
+      await tester.pump();
+      await tester.tap(find.byIcon(Icons.delete));
+      await tester.tap(find.text("Home"));
+      await tester.pump();
+
+      // Assert: should see the current word being unfavorited on the generator page
+      expect(find.byIcon(Icons.favorite_border), findsOneWidget);
     });
   });
 }
